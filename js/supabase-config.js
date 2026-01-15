6693f1e6c2d5f1e2eb97808b3820f0a9957eff19
alissonpires78 <alisson.schimanski@gmail.com>
2026-01-14 23:31:48 -0300

    Delete js/supabase-config.js

--- js/supabase-config.js
+++
@@ -1,6 +0,0 @@
-// Configurações do Supabase
-// Substitua pelas suas credenciais reais do Supabase
-const SUPABASE_URL = https://ksphuzzdidmcnpdrfgqz.supabase.co;
-const SUPABASE_KEY = sb_publishable_8m6xYeR6bn79J114Yr0B3w_k_c4nLIX;
-
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
