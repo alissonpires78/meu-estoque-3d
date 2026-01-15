dab750baaed713465ad7d69617fca78d41366c24
alissonpires78 <alisson.schimanski@gmail.com>
2026-01-15 09:59:36 -0300

    Delete assets directory

--- assets/js/data.source.js
+++
@@ -1,7 +0,0 @@
-// Troque para "supabase" quando migrar
-const DATA_SOURCE = "json"; // "json" | "supabase"
-
-async function loadFilamentosFromSource() {
-  if (DATA_SOURCE === "supabase") return await loadFilamentosFromSupabase();
-  return await loadFilamentosFromJson();
-}
